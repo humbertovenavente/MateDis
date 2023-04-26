d744d6557736b7e97cf79ba6c2324d30eab18fbb
humbertovenavente <humbertovenavente7@gmail.com>
2023-04-26 00:19:47 -0600

    Documentar base

--- new/convertBase.py
+++ new/convertBase.py
@@ -1,14 +1,15 @@
 
-#
+#CONVERTIMOS QUE SOLO ACEPTE DIGITOS DEL 0 AL 9 Y A AL F
 correspondenceString = r"""0123456789ABCDEFabcdef/-"""
 
+#pRIMERA DEFINICI´NO DE PASAR LAS BASES
 def convert(number = '10', baseFrom = 10, baseTo = 10):
     check = checks(number, baseFrom, baseTo)
     if not check.accepted:return check.message
     convertedToBase10 = convertToBase10(number, baseFrom)
     return convertToBase(convertedToBase10, baseTo)
 
-
+#cODIGO QUE AYUO A PASAR A BASE 10
 def convertToBase10(number = "10", baseFrom = 10, correspondenceString = correspondenceString):
     baseFrom = int(baseFrom)
     charToNumCorrespondence = {j:i for i, j in enumerate(correspondenceString)}
@@ -20,7 +21,7 @@ def convertToBase10(number = "10", baseFrom = 10, correspondenceString = corresp
     return convertedBase10
 
 
-
+#cODIGO EXTERNO PARA PASAR DE TODOS LAS LETRAS
 def convertToBase(number = "10", baseTo = 10, correspondenceString = correspondenceString):
     baseTo = int(baseTo)
     numToCharCorrespondence = {i:j for i, j in enumerate(correspondenceString)}
@@ -34,20 +35,20 @@ def convertToBase(number = "10", baseTo = 10, correspondenceString = corresponde
 
     return convertedNum[::-1]
 
-
+#dEFINICION QUE AYUDO A PODER IDENTIFICAR SI LA BASE ES VALIDA O NO, SE SOPORTA O NO
 def checks(number, baseFrom, baseTo, correspondenceString = correspondenceString):
     try:
         baseTo = int(baseTo);baseFrom = int(baseFrom)
     except Exception:return checkMessage(False, "base invalida")
     
-    if baseTo < 1 or baseFrom < 1:return checkMessage(False, "base must be greator then 0")
-    if baseTo > len(correspondenceString):return checkMessage(False, "this base is not supported")
+    if baseTo < 1 or baseFrom < 1:return checkMessage(False, "Base debe de ser mayor a 0")
+    if baseTo > len(correspondenceString):return checkMessage(False, "Base not supported")
     for i in str(number):
         if i not in correspondenceString:return checkMessage(False, "Caracter Invalido")
     
     return checkMessage(True, "Aceptado")
     
-
+#Se crea una clase aceptando mensaje
 class checkMessage:
     def __init__(self, accepted, message):
         self.accepted, self.message = accepted, message
